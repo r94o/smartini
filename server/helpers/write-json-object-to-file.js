9cb2ed77391995ff578b9c7a13b1819e90b9e4aa
Felix Taljaard <felixtaljaard@hotmail.com>
2022-05-03 17:02:24 +0100

    Database cleaned up

--- server/helpers/write-json-object-to-file.js
+++ server/helpers/write-json-object-to-file.js
@@ -1,87 +1,158 @@
-const fs = require("fs");
-const fetch = require("cross-fetch");
-const baseUrl = "https://www.thecocktaildb.com/api/json/v2/";
+const fs = require('fs');
+const fetch = require('cross-fetch');
 
-const apiKey = process.env.API_KEY || "1";
-console.log(apiKey);
+const baseUrl = 'https://www.thecocktaildb.com/api/json/v2/';
 
-const alphanumeric = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-];
-
-const allDrinks = []
-
-
-
-async function gatherDrinks(callback) {
-  for (let letter of alphanumeric) {
-    await fetch(
-      `https://www.thecocktaildb.com/api/json/v2/${apiKey}/search.php?` +
-      new URLSearchParams({
-        f: letter,
-      })
-    )
-    .then((response) => response.json())
-    .then((data) => {
-      if(data.drinks){
-        for (let drink of data.drinks) {
-          allDrinks.push(drink)
-        }
-      }
-    })
-  }
-  const jsonData = {drinks: []}
-  jsonData.drinks = allDrinks
-  const content = JSON.stringify(jsonData)
-  callback(content)
-}
-
-gatherDrinks((data) => {
-  writeData(data)    
-})
+const apiKey = process.env.API_KEY || '1';
 
 const writeData = (data) => {
-  fs.writeFile(`db.json`, data, "utf8", function (err) {
+  fs.writeFile('db.json', data, 'utf8', (err) => {
     if (err) {
-      console.log("tere was an error");
+      console.log('tere was an error');
       return console.log(err);
     }
-    console.log("JSON file was saved");
+    return console.log('JSON file was saved');
   });
 };
 
+const stringsToArray = (...strings) => strings.filter((string) => (string != null && string !== ''));
+
+const alphanumeric = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+];
+
+let allDrinks = [];
+
+async function gatherDrinks(callback) {
+  for(const letter of alphanumeric) {
+    await fetch(
+      `${baseUrl}${apiKey}/search.php?${
+        new URLSearchParams({
+          f: letter,
+        })}`,
+    )
+      .then((response) => response.json())
+      .then(({ drinks }) => {
+        if (drinks) {
+          const cleanedDrinks = drinks.map(({
+            strInstructionsDE, strInstructionsES, strInstructionsFR, strInstructionsIT, strInstructionsZH, strDrinkAlternate, strVideo, 'strInstructionsZH-HANS': hans, 'strInstructionsZH-HANT': hant, idDrink,
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+            strIngredient6,
+            strIngredient7,
+            strIngredient8,
+            strIngredient9,
+            strIngredient10,
+            strIngredient11,
+            strIngredient12,
+            strIngredient13,
+            strIngredient14,
+            strIngredient15,
+            strMeasure1,
+            strMeasure2,
+            strMeasure3,
+            strMeasure4,
+            strMeasure5,
+            strMeasure6,
+            strMeasure7,
+            strMeasure8,
+            strMeasure9,
+            strMeasure10,
+            strMeasure11,
+            strMeasure12,
+            strMeasure13,
+            strMeasure14,
+            strMeasure15, ...rest
+          }) => {
+            const ingredients = stringsToArray(
+              strIngredient1,
+              strIngredient2,
+              strIngredient3,
+              strIngredient4,
+              strIngredient5,
+              strIngredient6,
+              strIngredient7,
+              strIngredient8,
+              strIngredient9,
+              strIngredient10,
+              strIngredient11,
+              strIngredient12,
+              strIngredient13,
+              strIngredient14,
+              strIngredient15,
+            );
+            const measures = stringsToArray(
+              strMeasure1,
+              strMeasure2,
+              strMeasure3,
+              strMeasure4,
+              strMeasure5,
+              strMeasure6,
+              strMeasure7,
+              strMeasure8,
+              strMeasure9,
+              strMeasure10,
+              strMeasure11,
+              strMeasure12,
+              strMeasure13,
+              strMeasure14,
+              strMeasure15,
+            );
+            return {
+              id: idDrink,
+              ingredients,
+              measures,
+              ...rest,
+            };
+          });
+          allDrinks = allDrinks.concat(cleanedDrinks);
+        }
+      });
+  };
+  const jsonData = { drinks: [] };
+  jsonData.drinks = allDrinks;
+  const content = JSON.stringify(jsonData);
+  callback(content);
+}
+
+gatherDrinks((data) => {
+  writeData(data);
+});
