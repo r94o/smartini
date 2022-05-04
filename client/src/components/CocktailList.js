d45fca3443cd2b4e89e034df36bedaa52a655e33
Paul <pkilgarriff@gmail.com>
2022-05-04 19:23:49 +0100

    Add Cross Origin Resource Sharing
    Hardcode default back-end port to 3001

--- client/src/components/CocktailList.js
+++ client/src/components/CocktailList.js
@@ -1,31 +1,31 @@
 import { useEffect, useState } from 'react'
 
 const CocktailList = (props) => {
-  const [cocktails, setCocktails] = useState([]) 
+  const [cocktails, setCocktails] = useState([])
 
   useEffect(() => {
-      fetch("", {
-        method: "POST",
-        body: JSON.stringify({
-          ingredients: props.ingredients
-        }),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8"
-        }
-      })
+    fetch("http://localhost:3001/drinks", {
+      method: "POST",
+      body: JSON.stringify({
+        ingredients: props.ingredients
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
       .then(response => response.json())
-      .then(({drinks}) => setCocktails(drinks));
+      .then(({ drinks }) => setCocktails(drinks));
   }, [props.ingredients]);
 
   return (
     <div id="cocktail-list">
       <table>
-       <tbody>
-        {cocktails.map((cocktail, i) =>
-          <tr key={i}>
-            <td><img src={cocktail.strDrinkThumb} alt="cocktail" width="200" height="200"></img>{cocktail.strDrink}</td>
-          </tr>
-        )}
+        <tbody>
+          {cocktails.map((cocktail, i) =>
+            <tr key={i}>
+              <td><img src={cocktail.strDrinkThumb} alt="cocktail" width="200" height="200"></img>{cocktail.strDrink}</td>
+            </tr>
+          )}
         </tbody>
       </table>
     </div>
