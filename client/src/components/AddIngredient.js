4e90fb396528596d77df9100907f05639a222fc1
Felix Taljaard <felixtaljaard@hotmail.com>
2022-05-05 12:26:17 +0100

    Implemented basic autocomplete
    Co-authored-by: Rob Oman <r94o@users.noreply.github.com>

--- client/src/components/AddIngredient.js
+++ client/src/components/AddIngredient.js
@@ -1,16 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const AddIngredient = ({ addIngredient }) => {
   const [ingredientInput, setIngredientInput] = useState("")
+  const [matchingIngredients, setMatchingIngredients] = useState([])
+  const [allIngredients, setAllIngredients] = useState([])
+
+  useEffect(() => {
+    fetch("https://862840e1-6fa8-4a2d-a874-15705d2f04cb.mock.pstmn.io/get")
+      .then(response => response.json())
+      .then(( ingredients ) => setAllIngredients(ingredients));
+  }, []);
+  
 
   const handleChange = (event) => {
     setIngredientInput(event.target.value)
-  }
+    if (event.target.value.length < 2) {
+      setMatchingIngredients([])
+    } else {
+    const filter = allIngredients.filter(ingredient => {
+      return ingredient.name.includes(event.target.value)
+    })
+    setMatchingIngredients(filter)
+  }}
 
-  const handleClick = () => {
+  const handleClick = (event) => {
     if (!ingredientInput) return;
-    addIngredient(ingredientInput);
+    addIngredient(event.target.innerHTML);
     setIngredientInput("");
+    setMatchingIngredients([]);
   }
 
   const handleKeyDown = (event) => {
@@ -19,10 +36,17 @@ const AddIngredient = ({ addIngredient }) => {
   }
 
   return (
+    <>
     <div className="add-ingredient-container">
       <input type="text" id="ingredient-input" onChange={handleChange} onKeyDown={handleKeyDown} value={ingredientInput} placeholder="Enter Ingredient" />
       <button onClick={handleClick} id="add-ingredient">Add</button>
     </div>
+    <div id="dropdown">
+      <ul>
+        {matchingIngredients.map((ingredient, i) => (<li key={i} onClick={handleClick}>{ingredient.name}</li>))}
+      </ul>
+    </div>
+    </>
   )
 }
 
