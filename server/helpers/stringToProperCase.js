10af2224cb04773dff9f1fdc67393e3a5ab3e10e
henryferrari <hcvferrari@gmail.com>
2022-05-05 16:35:28 +0100

    removed profanity
    Co-authored-by: Paul Kilgarriff <PKilgarriff@users.noreply.github.com>

--- server/helpers/stringToProperCase.js
+++ server/helpers/stringToProperCase.js
@@ -1,6 +1,6 @@
 const drinks = [
-  { name: '1-900-fuk-me-up' },
-  { name: 'acid' },
+  { name: 'martini' },
+  { name: 'dry martini' },
 ];
 
 drinks.forEach((drink) => drink.name.replace(/\w\S*/g, (word) => (word.replace(/^\w/, (character) => character.toUpperCase()))));
