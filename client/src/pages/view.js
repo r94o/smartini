6f3627ec52d9cc9087e55fee6038cb154d4779d6
Rob Oman <rob.oman@icloud.com>
2022-05-09 13:03:36 +0100

    merge into main

--- client/src/pages/view.js
+++ client/src/pages/view.js
@@ -8,7 +8,7 @@ const View = () => {
 
 
       <div id="drinks-container">
-        <div class="card mb-3" style={{"max-width": "540px"}}>
+        <div class="card mb-3" style={{"max-width": "75%"}}>
           <div class="row g-0">
         <div class="col-md-4">
         <img src={drink.image} class="img-fluid rounded-start" alt="..." />
@@ -18,11 +18,20 @@ const View = () => {
         <div class="col-md-8">
           <div class="card-body">
             <h5 class="card-title">{drink.displayName}</h5>
-            <p class="card-text">This is a wider card with  a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit lo a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit lo a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit losupporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <ul>
+              {drink.ingredients.map((ingredient, i) => { 
+                let measure = ""
+                if (i < drink.measures.length){
+                  measure = drink.measures[i]
+                }  
+                return (<li key={i}>{ingredient}  {measure}</li>)
+              })}
+          </ul>
+          <p className="card-text">{drink.instructions}</p>
           </div>
         </div>
-      </div>
         </div>
+      </div>
         
         <img src={drink.image} alt="cocktail" width="200"/>
         <h1>{drink.displayName}</h1>
