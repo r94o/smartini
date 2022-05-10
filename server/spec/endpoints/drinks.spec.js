45cc3040b177bd5d796bd63450f06cd35c60e21d
Paul <pkilgarriff@gmail.com>
2022-05-10 20:16:04 +0100

    Update /drinks endpoint test

--- server/spec/endpoints/drinks.spec.js
+++ server/spec/endpoints/drinks.spec.js
@@ -8,33 +8,30 @@ describe('Drinks endpoint', () => {
     it('should return a 200 status code and an array of drinks', async () => {
       const response = await request(baseUrl)
         .get('drinks');
-      expect(response.statusCode).toBe(200);
 
       const firstObject = response.body.drinks[0];
 
-      await waitForExpect(() => {
-        expect(firstObject).toHaveProperty('_id');
-        expect(firstObject).toHaveProperty('ingredients');
-        expect(firstObject).toHaveProperty('measures');
-        expect(firstObject).toHaveProperty('name');
-        expect(firstObject).toHaveProperty('displayName');
-        expect(firstObject).toHaveProperty('category');
-        expect(firstObject).toHaveProperty('iba');
-        expect(firstObject).toHaveProperty('alcoholic');
-        expect(firstObject).toHaveProperty('glass');
-        expect(firstObject).toHaveProperty('instructions');
-        expect(firstObject).toHaveProperty('image');
-      });
-
+      expect(response.statusCode).toBe(200);
+      expect(firstObject).toHaveProperty('_id');
+      expect(firstObject).toHaveProperty('ingredients');
+      expect(firstObject).toHaveProperty('measures');
+      expect(firstObject).toHaveProperty('name');
+      expect(firstObject).toHaveProperty('displayName');
+      expect(firstObject).toHaveProperty('category');
+      expect(firstObject).toHaveProperty('iba');
+      expect(firstObject).toHaveProperty('alcoholic');
+      expect(firstObject).toHaveProperty('glass');
+      expect(firstObject).toHaveProperty('instructions');
+      expect(firstObject).toHaveProperty('image');
       const objectsCount = response.body.drinks.length;
       expect(objectsCount).not.toEqual([null]);
     });
   });
-  describe('FilterByIdString', () => {
+  describe('FindByName', () => {
     it('returns the drink if it exists', async () => {
-      const id = 11007; // Id String for Margarita
+      const searchQuery = encodeURI('margarita');
       const response = await request(baseUrl)
-        .get(`drinks/${id}`);
+        .get(`drinks/name/${searchQuery}`);
       expect(response.statusCode).toBe(200);
 
       const objectsCount = response.body.drinks.length;
@@ -42,15 +39,12 @@ describe('Drinks endpoint', () => {
 
       const firstObject = response.body.drinks[0];
       const margaritaObject = {
-        id: '11007',
-        ingredients: ['tequila', 'triple sec', 'lime juice', 'salt'],
         measures: ['1 1/2 oz ', '1/2 oz ', '1 oz '],
         name: 'margarita',
         displayName: 'Margarita',
         category: 'Ordinary Drink',
         iba: 'Contemporary Classics',
         alcoholic: 'Alcoholic',
-        glass: 'Cocktail glass',
         instructions: 'Rub the rim of the glass with the lime slice to make the salt stick to it. Take care to moisten only the outer rim and sprinkle the salt on it. The salt should present to the lips of the imbiber and never mix into the cocktail. Shake the other ingredients with ice, then carefully pour into the glass.',
         image: 'https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg',
       };
@@ -59,9 +53,9 @@ describe('Drinks endpoint', () => {
       });
     });
     it('returns an empty array if it does not exist', async () => {
-      const id = 'a1b1c0d0e7'; // Id String that isn't in the database
+      const searchQuery = encodeURI('this does not exist'); // Id String that isn't in the database
       const response = await request(baseUrl)
-        .get(`drinks/${id}`);
+        .get(`drinks/name/${searchQuery}`);
       expect(response.statusCode).toBe(200);
       expect(response.body.drinks).toEqual([null]);
     });
