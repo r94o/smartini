d086739abef3a86bf63c386ec20d9a2a1deabcbd
Paul <pkilgarriff@gmail.com>
2022-05-11 12:52:20 +0100

    Initial setup of FilterByOneIngredientMissing route
    Co-authored-by: Henry Ferrari <HCVF@users.noreply.github.com>

--- server/controllers/drinks.js
+++ server/controllers/drinks.js
@@ -63,16 +63,28 @@ const DrinksController = {
       res.sendStatus(500);
     }
   },
+  FilterByOneIngredientMissing: (req, res) => {
+    const queryIngredients = req.body.ingredients.map((ingredient) => ingredient.toLowerCase());
+    try {
+      Drink.find({ ingredients: { $in: searchIngredient } })
+        .populate("glass")
+        .then((drinks) => {
+          res.json({ drinks });
+        });
+    } catch (e) {
+      res.sendStatus(500);
+    }
+  },
   FindVideoByName: (req, res) => {
     const drinkName = encodeURI(req.params.name);
     fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=how%20to%20make%20${drinkName}%20cocktail&type=video&key=${process.env.YOUTUBE_API_KEY}`)
-    .then(response => response.json())
-    .then(data => {
-      if (data.items) {
-        const videoId = data.items[0].id.videoId
-        res.json({ videoId })
-      }
-    });
+      .then(response => response.json())
+      .then(data => {
+        if (data.items) {
+          const videoId = data.items[0].id.videoId
+          res.json({ videoId })
+        }
+      });
   }
 };
 
