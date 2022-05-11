3d24c7af19ccf9948f819129d10900aab36e00ea
Felix Taljaard <felixtaljaard@hotmail.com>
2022-05-11 10:55:50 +0100

    Hover zoom on matching drinks image working

--- client/src/components/MatchingDrinks.js
+++ client/src/components/MatchingDrinks.js
@@ -32,10 +32,13 @@ const MatchingDrinks = ({ ingredients, setDrink }) => {
       {drinks.map((drink, i) => (
         <Card sx={{ maxWidth: 150 }} className="matching-drink" key={i} onClick={() => handleClick(i)}>
           <CardActionArea >
+            <div className="img-wrapper">
             <CardMedia
               component="img"
               image={drink.image}
+              className="hover-zoom"
             />
+            </div>
             <CardContent>
               <Typography gutterBottom variant="h6" component="div">
                 {drink.displayName}
